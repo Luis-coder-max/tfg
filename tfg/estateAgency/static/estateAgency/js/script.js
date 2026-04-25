87afca4a91508420a14cc27d23acfe84683d0895
Luis-coder-max <lgmonteiro.e@gmail.com>
2026-04-25 21:22:37 +0200

    various fixes

--- tfg/estateAgency/static/estateAgency/js/script.js
+++ tfg/estateAgency/static/estateAgency/js/script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript conectado correctamente");
+function predict() {
+  const type = document.getElementById("type").value;
+  const city = document.getElementById("city").value;
+  const result = document.getElementById("result");
+
+  let message = "";
+
+  if (!city) {
+    result.innerHTML = "Introduce una ciudad";
+    return;
+  }
+
+  if (type === "compra") {
+    message = `En ${city}, se espera una subida de precios del 5-8% en los próximos meses.`;
+  } else if (type === "larga") {
+    message = `En ${city}, el alquiler de larga estancia seguirá en alta demanda con subidas moderadas.`;
+  } else {
+    message = `En ${city}, el alquiler de temporada tendrá picos altos en fechas turísticas.`;
+  }
+
+  result.innerHTML = message;
+}
